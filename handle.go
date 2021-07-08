9e03231bd173c71c692f09a546a7de91d53c77b6
Pasi Hyvönen <pasi@koud.fi>
2021-07-08 17:13:37 +0300

    Add handle utility function

---
+++ handle.go
@@ -0,0 +1,21 @@
+package serve
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request, fn func() (int64, error)) {
+	startTime := time.Now()
+
+	// TODO: improve size logging (both incoming and outgoing)
+
+	n, err := fn()
+	if n == 0 && err != nil {
+		Error(w, err)
+		log.Printf("[%d] %s %s (%v) ERROR: %v", statusCode(err), r.Method, r.URL, time.Since(startTime), err)
+	} else {
+		log.Printf("[200] %s %s (%d bytes, %v)", r.Method, r.URL, n, time.Since(startTime))
+	}
+}
