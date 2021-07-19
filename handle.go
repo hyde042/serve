279650ab43028737de6a2225eb50d8ff2546663b
Pasi Hyvönen <pasi@koud.fi>
2021-07-19 11:02:27 +0300

    Truncate massive URLs when logging

--- handle.go
+++ handle.go
@@ -10,13 +10,16 @@ func Handle(w http.ResponseWriter, r *http.Request, fn func() (int64, error)) {
 	startTime := time.Now()
 
 	// TODO: improve size logging (both incoming and outgoing)
-	// TODO: truncate massive URLs for logging
 
+	logUrl := r.URL.String()
+	if len(logUrl) > 120 {
+		logUrl = logUrl[:117] + "..."
+	}
 	n, err := fn()
 	if n == 0 && err != nil {
 		Error(w, err)
-		log.Printf("[%d] %s %s (%v) ERROR: %v", statusCode(err), r.Method, r.URL, time.Since(startTime), err)
+		log.Printf("[%d] %s %s (%v) ERROR: %v", statusCode(err), r.Method, logUrl, time.Since(startTime), err)
 	} else {
-		log.Printf("[200] %s %s (%d bytes, %v)", r.Method, r.URL, n, time.Since(startTime))
+		log.Printf("[200] %s %s (%d bytes, %v)", r.Method, logUrl, n, time.Since(startTime))
 	}
 }
